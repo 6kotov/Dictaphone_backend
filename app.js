e43925c453ba8871f46b800aa11211b3962fb646
6kotov <66kotov@gmail.com>
2020-04-21 17:43:30 +0200

    Uploading & downloading APIs added.

--- app.js
+++ app.js
@@ -7,31 +7,55 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 require("fs").promises;
+
 const bodyParser = require("body-parser");
-const Storage = multer.diskStorage({
+const imageStorage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, "./images");
+    callback(null, "./public/images/");
   },
   filename(req, file, callback) {
     callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
   },
 });
-const upload = multer({ storage: Storage });
+const soundStorage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "./public/sounds/");
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
+const imageUpload = multer({ storage: imageStorage });
+const soundUpload = multer({ storage: soundStorage });
 
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static("public"));
 
+if (!fs.existsSync("./public/images/")) {
+  fs.mkdirSync("./public/images/");
+}
+
+if (!fs.existsSync("./public/sounds/")) {
+  fs.mkdirSync("./public/sounds/");
+}
+
 app.get("/", async (req, res) => {
   const images = await readDirectory("./public/images/");
   const sounds = await readDirectory("./public/sounds/");
   const soundList = sounds.length ? sounds : ["No sounds loaded!"];
   const imageList = images.length ? images : ["No images loaded!"];
-  res.render("index", { imageList, soundList, aalert });
+  res.render("index", { imageList, soundList });
 });
 
-app.post("/api/upload", upload.single("photo"), (req, res) => {
+app.post("/api/upload/image/", imageUpload.single("photo"), (req, res) => {
+  res.status(200).json({
+    status: "success",
+  });
+});
+
+app.post("/api/upload/sound/", soundUpload.single("sound"), (req, res) => {
   res.status(200).json({
     status: "success",
   });
@@ -47,7 +71,8 @@ app.get("/api/file", async (req, res, next) => {
     },
   };
   const images = await readDirectory("./public/images/");
-  var fileName = images[0];
+  const fileName = images[0];
+
   res.sendFile(fileName, options, function (err) {
     if (err) {
       next(err);
