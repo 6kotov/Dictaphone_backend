30b11657e435c85b9e36142b5d72fce199763485
6kotov <66kotov@gmail.com>
2020-04-21 16:11:27 +0200

    Simple server implemented

--- app.js
+++ app.js
@@ -1,74 +1,68 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+require("fs").promises;
+const bodyParser = require("body-parser");
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "./images");
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
+const upload = multer({ storage: Storage });
 
-const express = require("express")
-const app = express()
-const mongoose = require('mongoose')
-const User = require('./models/users')
-const multer  = require('multer')
-const upload = multer({ dest: 'tmp/csv/' })
-const csv = require('fast-csv');
-const fs = require('fs');
-const Json2csvParser = require('json2csv').Parser;
-
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
-app.set('views',__dirname+'/views')
-app.use('/public', express.static('public'));
+app.set("views", __dirname + "/views");
+app.use("/public", express.static("public"));
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,  useUnifiedTopology: true  })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open',async () => {
-    console.log('Connected to Mongoose')
-})
-mongoose.set('useFindAndModify', false);
-
-app.get('/', (req, res) =>{
-    res.render('index')
-})
-
-app.get('/users_list', async (req, res) =>{
-    let users =  await User.find()
-    res.render('users_list',{users:users})
-})
-
-app.post('/upload', upload.single('csv_upload'), async (req, res) =>{
-    let fileRows = [];
-
-  await  csv.parseFile(req.file.path)
-        .on("data", function (data) {
-            fileRows = data;
-        })
-        .on("end", async function () {
-            fs.unlinkSync(req.file.path);
-
-            const user = new User({
-                userName: fileRows[0],
-                firstName: fileRows[1],
-                lastName: fileRows[2],
-                age:fileRows[3],
-                married:fileRows[4]
-            })
-            try {
-                await user.save()
-            } catch (e){
-                console.log({message: e.message})
-            }
-            res.render('index')
-        })
-
+app.get("/", async (req, res) => {
+  const images = await readDirectory("./public/images/");
+  const sounds = await readDirectory("./public/sounds/");
+  const soundList = sounds.length ? sounds : ["No sounds loaded!"];
+  const imageList = images.length ? images : ["No images loaded!"];
+  res.render("index", { imageList, soundList, aalert });
 });
 
-app.get('/load_users', async (req,res)=>{
-    const fields = ['userName', 'firstName', 'lastName', 'age' , 'married'];
-    const users =  await User.find()
-    const json2csvParser = new Json2csvParser({ fields: fields, quote: ''});
-    const csv = await json2csvParser.parse(users);
-    fs.writeFileSync('./users.csv', csv, function (err) {
-        if (err) throw err;
-    });
-    res.download(__dirname+'/users.csv')
-})
+app.post("/api/upload", upload.single("photo"), (req, res) => {
+  res.status(200).json({
+    status: "success",
+  });
+});
 
-app.listen(process.env.PORT ||3000, console.log('Server started'))
+app.get("/api/file", async (req, res, next) => {
+  var options = {
+    root: path.join(__dirname, "public/images/"),
+    dotfiles: "deny",
+    headers: {
+      "x-timestamp": Date.now(),
+      "x-sent": true,
+    },
+  };
+  const images = await readDirectory("./public/images/");
+  var fileName = images[0];
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
+
+async function readDirectory(dirPath) {
+  const directoryPath = path.join(__dirname, dirPath);
+  return fs.promises.readdir(directoryPath);
+}
+
+app.listen(
+  process.env.PORT || 3000,
+  console.log("Server started on http://localhost:3000")
+);
